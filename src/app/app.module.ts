6e0dede214fe25ddaaa9aaaddb6a52ea8acdca9e
tauseefjanjua <tauseef.a.janjua@gmail.com>
2020-12-03 01:45:08 +0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
